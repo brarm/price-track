bb75603ca3b84f711e7cc83934be8714e2e0c4c5
EC2 Default User <ec2-user@ip-172-31-9-154.us-east-2.compute.internal>
2019-11-03 09:19:59 +0000

    Make price track script ready for deploy

--- price-track.py
+++ price-track.py
@@ -17,12 +17,13 @@ context = ssl.create_default_context()
 sender_email = 'price.tracker.project.accnt@gmail.com'
 receiver_email = "brargolf@gmail.com"
 
-def send_email(link, old_price, now_price):
+def send_email(link=None, old_price=None, now_price=None, message=None):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender_email, password)
         # parser.parse(strtime)
         # datetime.datetime(2012, 11, 13, 17, 2, 22, 395000)
-        message = f'''\
+        if not message:
+            message = f'''\
 Subject: Price alert
 
 
@@ -37,21 +38,23 @@ Go to {link} to check it out.'''
         # Send email here
         server.sendmail(sender_email, receiver_email, message)
 
-prices = [{'time':str(datetime.datetime.now()),'price':200.00}]
-
 def get_price_from_string(s):
     m = re.search(r'\$([0-9]+[\.]*[0-9]*)', s)
     return float(m.group()[1:]) if m else None
 
+prices = [{'time':str(datetime.datetime.now()),'price':0.00}]
 counter = 0
 
 while True:
+    counter += 1
     link =  'https://www.orvis.com/p/weatherbreaker-jacket/1z5b'
     response = requests.get(link)
     if response.status_code != 200:
-        print(response.status_code)
-        print(response.text)
+        with open('logfile', 'a+') as log_file:
+            logfile.write(response.status_code)
+            logfile.write(response.text)
         continue
+
     soup = BeautifulSoup(response.text, 'html.parser')
     non_sale_element = soup.find_all('span', attrs = {'class' : 'MemPrice'})
     sale_element = soup.find_all('span', attrs = {'class' : 'MemSalePrice'})
@@ -63,12 +66,29 @@ while True:
 
     old_price = prices[-1]
     now_price = {'time' : str(datetime.datetime.now()),'price' : price}
-    
+
     if price < old_price['price']:
-        send_email(link, old_price, now_price)
-        break
+        send_email(link=link, old_price=old_price, now_price=now_price)
+        counter = 0
+
+    elif counter % 24 == 0:
+        # send daily update
+        message=f'''\
+Subject: Price is steady
+
+
+The price of your tracked item is steady.
+${price:.2f} (as of {now_price['time']}
+
+View the item at {link}'''
+        send_email(link=link, message=message)
+        counter = 0
+    
     prices.append(now_price)
 
-    time.sleep(1)
+    with open('output', 'a+') as out_file:
+        out_file.write(f'${now_price["price"]:.2f now_price["time"]')
+    # sleep for 1 hour
+    time.sleep(60*60)
 
 
